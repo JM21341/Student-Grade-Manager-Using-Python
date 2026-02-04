31104b821131f1936a7c567b2375e021acdfa044
JM21341 <fjohnmiguel@gmail.com>
2026-02-04 18:47:07 +0800

    Found a solution for classes.py

--- classes.py
+++ classes.py
@@ -1,14 +1,10 @@
-class Employee:
-    num_of_emp = 0 # counts the number of employees
-
-    def __init__(self, name, age, salary, tax, next):
-        self.name = name
-        self.age = age
-        self.salary = salary
-        self.tax = tax / 100
-        self.next = None
-        Employee.num_of_emp += 1
-    
-class Teacher(Employee):
-    def __init__(self):
+class PersonalData:
+    def __init__(self, name, age, birthdate, fb, ig, ):
         pass
+        
+
+class LoginData:
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
--- test files/test.py
+++ test files/test.py
@@ -1,3 +1,21 @@
-import classes 
+import class_test as ctest
 
-if name == "__main__"
+def insertNode(head, data):
+    new_node = ctest.Node(data)
+
+    if head is None:
+        head = new_node
+    else:
+        curr = head
+        # linked list traversal
+        while curr.Node.next is not None:
+            curr = curr.Node.next
+        
+        curr.next = new_node # sets the next node of the last node as the new node
+
+        new_node.prev = curr
+    
+    return head
+
+if __name__ == "__main__":
+    pass
