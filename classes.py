a495acaa52a86c2dc5f8ab3f6cccd530bbf7be77
JM21341 <fjohnmiguel@gmail.com>
2026-02-05 19:19:42 +0800

    Added node class

--- classes.py
+++ classes.py
@@ -19,3 +19,12 @@ class LoginData:
 class EmployeeData:
     def __init__(self, salary, tax, occupation, title):
         self.monthly_salary = salary - (salary * (tax / 100))
+        self.occupation = occupation
+        self.title = title 
+
+class Node:
+    def __init__(self, login_data, personal_data, emp_data):
+        self.login_data = login_data
+        self.personal_data = personal_data
+        self.employee_data = emp_data
+        self.next = None 
